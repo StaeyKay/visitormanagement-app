f9ed3e040a18a09260e4346b0caff9fb8f4792e0
StaeyKay <ekwashie001@st.ug.edu.gh>
2024-11-27 19:03:15 +0000

    modified the check in form

--- src/pages/checkin.jsx
+++ src/pages/checkin.jsx
@@ -48,71 +48,67 @@ const Checkin = () => {
       <h1 className="bg-[#E62E2D] text-center text-white py-4 font-bold text-[40px]">
         Welcome to Graphic Communications Group Limited
       </h1>
-      <div className="p-10">
-        <div className="bg-[#dfa2a2] p-10 rounded-md space-y-4">
+      <div className="px-60 py-10">
+        <div className="bg-[#dfa2a2] py-10 px-20 rounded-md space-y-4">
           <h4>Check-In Form</h4>
           <p className="text-white">Please fill in the form below</p>
           <form
             onSubmit={handleForm}
             action=""
-            className="flex flex-col space-y-4"
+            className="grid grid-cols-2 gap-5 items-start"
           >
-            <label htmlFor="" className="text-white">
-              Your full name:
-            </label>
-            <input
-              className="rounded-md h-8 p-2"
-              type="text"
-              placeholder="Full Name"
-              onChange={(e) => setVisitorName(e.target.value)}
-              value={visitorName}
-              required
-            />
-            <label htmlFor="" className="text-white">
-              Your phone number
-            </label>
-            <input
-              className="rounded-md h-8 p-2"
-              type="text"
-              placeholder="Enter your phone number"
-              onChange={(e) => setPhone(e.target.value)}
-              value={phone}
-              required
-            />
-            <label htmlFor="" className="text-white">
-              Host:
-            </label>
-            <input
-              className="rounded-md h-8 p-2"
-              type="text"
-              placeholder="Full Name"
-              onChange={(e) => setEmployeeName(e.target.value)}
-              value={employeeName}
-              required
-            />
-            <label htmlFor="" className="text-white">
-              Purpose of visit
-            </label>
-            <input
-              className="rounded-md h-8 p-2"
-              type="text"
-              placeholder="Purpose"
-              onChange={(e) => setPurpose(e.target.value)}
-              value={purpose}
-            />
-            {/* <label htmlFor="" className="text-white">
-              Arrival time:
-            </label>
-            <input
-              className="rounded-md h-8"
-              type="datetime-local"
-              onChange={(e) => setTime(e.target.value)}
-              value={time}
-            /> */}
-            <button
-              // onClick={() => navigate("/checkout")}
-              className="bg-[#f1d5da] w-[100px] rounded-md"
-            >
+            <div className="flex flex-col">
+              <label htmlFor="" className="text-white">
+                Your full name
+              </label>
+              <input
+                className="rounded-md h-8 p-2"
+                type="text"
+                placeholder="Full Name"
+                onChange={(e) => setVisitorName(e.target.value)}
+                value={visitorName}
+                required
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="" className="text-white">
+                Your phone number
+              </label>
+              <input
+                className="rounded-md h-8 p-2"
+                type="text"
+                placeholder="Enter your phone number"
+                onChange={(e) => setPhone(e.target.value)}
+                value={phone}
+                required
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="" className="text-white">
+                Host:
+              </label>
+              <input
+                className="rounded-md h-8 p-2"
+                type="text"
+                placeholder="Full Name"
+                onChange={(e) => setEmployeeName(e.target.value)}
+                value={employeeName}
+                required
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="" className="text-white">
+                Purpose of visit
+              </label>
+              <input
+                className="rounded-md h-8 p-2"
+                type="text"
+                placeholder="Purpose"
+                onChange={(e) => setPurpose(e.target.value)}
+                value={purpose}
+              />
+            </div>
+            <button className="bg-[#f1d5da] w-[100px] rounded-md">
               Check In
             </button>
           </form>
